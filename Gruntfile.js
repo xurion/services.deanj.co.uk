6f89c6b5c95f05a68e2448fbad4fa40ac0d84689
Dean James <djames@brighttalk.com>
2016-06-24 18:27:17 +0100

    Switch to bower for html5shiv and respondJS. Corrected font copy task in Grunt. Working on Styling contact form properly.

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,13 +32,26 @@ module.exports = function(grunt) {
             fonts: {
                 files: [{
                     expand: true,
-                    cwd: 'font-awesome/fonts',
+                    cwd: 'bower_components/font-awesome/fonts',
                     src: ['*'],
                     dest: 'fonts/',
                     filter: 'isFile'
                 }]
             },
-        },
+            libs: {
+                files: [{
+                    expand: true,
+                    cwd: 'bower_components',
+                    src: [
+                        'html5shiv/dist/html5shiv.min.js',
+                        'respond/dest/respond.min.js'
+                    ],
+                    dest: 'js/',
+                    filter: 'isFile',
+                    flatten: true
+                }]
+            }
+        }
     });
 
     // Load the plugins.
